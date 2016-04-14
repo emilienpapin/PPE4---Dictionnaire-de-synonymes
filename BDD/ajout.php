9971bf6b910b6fc31c51d5ad2c1115a0b55d7337
emilienpapin <emilien.papin@bts-malraux.net>
2016-04-14 23:49:52 +0200

    FIX #1 - Ajout mot

---
+++ BDD/ajout.php
@@ -0,0 +1,28 @@
+<?php
+  include("connexion.php");
+?>
+
+<html>
+    <head><title>Ajouter in mot</title></head>
+    <body>
+      <form name="inscription" method="post" action="ajout.php">
+            Entrez le mot : <input type="text" name="mot"/> <br/>
+
+            <input type="submit" name="valider" value="OK"/>
+        </form>
+      </body>
+</html>
+
+<?php
+  if(isset($_POST['valider'])){
+            $mot=$_POST['mot'];
+            $connexion->query("
+              INSERT INTO mot VALUES ('', '$mot');
+            ");
+
+  }
+?>
+
+
+</html>
+</body>
